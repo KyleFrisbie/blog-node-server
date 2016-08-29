4ce07802d1a8424666d197c9adf1cf41dbc50bae
Kyle L Frisbie <kyle.l.frisbie@gmail.com>
2016-08-28 19:45:56 -0600

    revert back to 'pre-travis attempted changes'

--- test/test-blogposts.js
+++ test/test-blogposts.js
@@ -76,45 +76,41 @@ describe('BlogPost-Test', function () {
       });
   });
 
-  describe('retries', function () {
-    this.retries(4);
-
-    it('should update a blog post with new values to /api/update-blogpost POST', function (done) {
-      var blogpost = fakeBlogpost();
-      blogpost.save();
-      var newTitle = faker.lorem.sentence();
-      blogpost.title = newTitle;
-      blogpost.tags = [];
-      chai.request(server)
-        .put('/api/blogposts/update-blogpost')
-        .send(blogpost)
-        .end(function (err, res) {
-          res.should.have.status(200);
-          res.should.be.json;
-          res.body.should.be.a('object');
-          res.body.should.have.property('success');
-          res.body.success.should.equal(true);
-          res.body.should.have.property('blogpost');
-          res.body.blogpost.should.be.a('object');
-          res.body.blogpost.should.have.property('title');
-          res.body.blogpost.title.should.equal(newTitle);
-          res.body.blogpost.should.have.property('subtitle');
-          res.body.blogpost.subtitle.should.equal(blogpost.subtitle);
-          res.body.blogpost.should.have.property('author');
-          res.body.blogpost.author.should.equal(blogpost.author);
-          res.body.blogpost.should.have.property('imageURL');
-          res.body.blogpost.imageURL.should.equal(blogpost.imageURL);
-          res.body.blogpost.should.have.property('postBody');
-          res.body.blogpost.postBody.should.equal(blogpost.postBody);
-          res.body.blogpost.should.have.property('createdOn');
-          // TODO: figure out how to implicitly convert blogpost date
-          //res.body.blogpost.createdOn.should.equal(blogpost.createdOn);
-          res.body.blogpost.should.have.property('tags');
-          res.body.blogpost.tags.should.be.a('Array')
-            .and.have.lengthOf(0);
-          done();
-        });
-    });
+  it('should update a blog post with new values to /api/update-blogpost POST', function (done) {
+    var blogpost = fakeBlogpost();
+    blogpost.save();
+    var newTitle = faker.lorem.sentence();
+    blogpost.title = newTitle;
+    blogpost.tags = [];
+    chai.request(server)
+      .put('/api/blogposts/update-blogpost')
+      .send(blogpost)
+      .end(function (err, res) {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('success');
+        res.body.success.should.equal(true);
+        res.body.should.have.property('blogpost');
+        res.body.blogpost.should.be.a('object');
+        res.body.blogpost.should.have.property('title');
+        res.body.blogpost.title.should.equal(newTitle);
+        res.body.blogpost.should.have.property('subtitle');
+        res.body.blogpost.subtitle.should.equal(blogpost.subtitle);
+        res.body.blogpost.should.have.property('author');
+        res.body.blogpost.author.should.equal(blogpost.author);
+        res.body.blogpost.should.have.property('imageURL');
+        res.body.blogpost.imageURL.should.equal(blogpost.imageURL);
+        res.body.blogpost.should.have.property('postBody');
+        res.body.blogpost.postBody.should.equal(blogpost.postBody);
+        res.body.blogpost.should.have.property('createdOn');
+        // TODO: figure out how to implicitly convert blogpost date
+        //res.body.blogpost.createdOn.should.equal(blogpost.createdOn);
+        res.body.blogpost.should.have.property('tags');
+        res.body.blogpost.tags.should.be.a('Array')
+          .and.have.lengthOf(0);
+        done();
+      });
   });
 
   it('should remove a blog post from the collection by /api/blogposts/remove-blogpost', function (done) {
